c965289d9df37693e24a7ce1cce9c6515c40f09e
Ray Walters <ray.walters@gmail.com>
2018-09-03 21:56:40 -0400

    Add 'show all' and 'show unplayed' functionality, and refactor data type

--- lib/emcee/route.rb
+++ lib/emcee/route.rb
@@ -7,22 +7,40 @@ module Emcee
           title  = $~.named_captures["title"]
           artist = $~.named_captures["artist"]
 
-          raise Emcee::DuplicateTitleError.new(title) if library[:_titles][title]
-          library[:_titles][title] = { played: false }
-
-          library[artist] ||= []
-          library[artist] << title
+          raise Emcee::DuplicateTitleError.new(title) unless library[title].nil?
+          library[title] = { artist: artist, played: false }
 
           message = %Q[Added "#{title}" by #{artist}]
           {message: message, library: library}
         when /^\s*play \"(?<title>[^\"]+)\"\s*$/i
           title = $~.named_captures["title"]
 
-          raise Emcee::TitleNotFoundError.new(title) unless library[:_titles][title]
+          raise Emcee::TitleNotFoundError.new(title) if library[title].nil?
 
-          library[:_titles][title][:played] = true
+          library[title][:played] = true
 
           { message: %Q[You're listening to "#{title}"], library: library }
+        when /^\s*show all\s*$/i
+          msg =
+            library.each.map do |elem|
+              title = elem.first
+              artist = elem.last[:artist]
+              played = elem.last[:played] ? "played" : "unplayed"
+
+              %Q["#{title}" by #{artist} (#{played})]
+            end.join("\n")
+
+            { message: msg, library: library }
+        when /^\s*show unplayed\s*$/i
+          msg =
+            library.each.reject{|elem| elem.last[:played] }.map do |elem|
+              title = elem.first
+              artist = elem.last[:artist]
+
+              %Q["#{title}" by #{artist}]
+            end.join("\n")
+
+            { message: msg, library: library }
         end
       end
     end
--- spec/lib/emcee/route_spec.rb
+++ spec/lib/emcee/route_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 RSpec.describe Emcee::Route do
   subject { described_class.parse(cmd, library) }
 
-  let(:library) { { _titles: {} } }
+  let(:library) { {  } }
   let(:title)   { "A Night at the Opera" }
   let(:artist)  { "Queen" }
 
   context "add an album" do
     let(:cmd) { %Q[add "#{title}" "#{artist}"] }
     let(:msg) { %Q[Added "#{title}" by #{artist}] }
-    let(:new_lib) { { _titles: { title => {played: false} }, "Queen" => [title] } }
+    let(:new_lib) { { title => {artist: "Queen", played: false} } }
 
     it "adds the new album" do
       expect(subject).to eq({ message: msg, library: new_lib })
@@ -28,8 +28,12 @@ RSpec.describe Emcee::Route do
   context "play an album" do
     let(:cmd) { %Q[play "#{title}"] }
     let(:msg) { %Q[You're listening to "#{title}"] }
-    let(:library) { { _titles: { title => {played: false} }, "Queen" => [title] } }
-    let(:new_lib) { { _titles: { title => {played: true} }, "Queen" => [title] } }
+    let(:library) do
+      { title => { played: false, artist: "Queen" } }
+    end
+    let(:new_lib) do
+      { title => { played: true, artist: "Queen" } }
+    end
 
     it "marks the album as played" do
       expect(subject).to eq({ message: msg, library: new_lib })
@@ -44,9 +48,64 @@ RSpec.describe Emcee::Route do
     end
   end
 
-  context "show all albums"
-  context "show unplayed albums"
-  context "show all albums by an artist"
-  context "show unplayed albums by artist"
+  context "show" do
+    context "show all" do
+      let(:cmd) { %Q[show all] }
+      let(:library) do
+        {
+          title => {artist: "Queen", played: false},
+          "Sheer Heart Attack"  => { artist: "Queen", played: true },
+          "News of the World"   => { artist: "Queen", played: false },
+          "Night Work" => { artist: "Scissor Sisters", played: true},
+        }
+      end
+
+      let(:msg) do
+        library.keys.map do |title|
+          artist = library[title][:artist]
+          played = library[title][:played] ? "played" : "unplayed"
+
+          %Q["#{title}" by #{artist} (#{played})]
+        end
+        .join("\n")
+      end
+
+      it "lists all albums and played status" do
+        expect(subject).to eq( { message: msg, library: library } )
+      end
+    end
+
+    context "show unplayed" do
+      let(:cmd) { %Q[show unplayed] }
+      let(:library) do
+        {
+          title => {artist: "Queen", played: false},
+          "Sheer Heart Attack"  => { artist: "Queen", played: true },
+          "News of the World"   => { artist: "Queen", played: false },
+          "Night Work" => { artist: "Scissor Sisters", played: true},
+        }
+      end
+
+      let(:msg) do
+        library
+          .keys
+          .reject{|k| library[k][:played] }
+          .map do |title|
+            artist = library[title][:artist]
+
+            %Q["#{title}" by #{artist}]
+          end
+          .join("\n")
+      end
+
+      it "lists all albums and played status" do
+        expect(subject).to eq( { message: msg, library: library } )
+      end
+    end
+
+    context "show all albums by an artist"
+    context "show unplayed albums by artist"
+  end
+
   context "quit the terminal"
 end
